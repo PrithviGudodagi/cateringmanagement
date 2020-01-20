621a442a07db5ae7faa10feb0feb12266602c28d
prithvigg <prithvigudodagi29@gmail.com>
2020-01-20 23:06:31 +0530

    latest

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var alert = require("alert-node");
+
 var express     = require('express');
 var app         = express();
 var port        = process.env.PORT || 3000;
@@ -16,11 +18,154 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.urlencoded({extended : true}))
 app.use(methodoverride("_method"));
 
+var Food = require("./app/models/food.js");
+var Order = require("./app/models/order.js");
+var Allorder = require("./app/models/allorder.js");
+//variables for datbase trasfer
+
+var foodinputname;
+var foodinputprice; 
+var orderfoodinputname;
+var orderfoodinputprice;
+var Token = 0 ;
+var ordertransfername;
+var ordertransferprice;
+
 app.get("/",(req,res)=>{
     res.render("main");
 });
 
-require("./app/routes.js")(app);
+    app.get("/index",(req,res)=>{
+        Food.find({},function(err,food){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("index",{ food : food });
+            }
+        });
+        
+    });
+
+    app.get("/cart",(req,res)=>{
+        Order.find({},function(err,orders){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("cart" , {order : orders });
+            }
+        });
+        
+    });
+
+  
+
+
+
+app.get("/out",(req,res)=>{
+    Token = Token + 1;
+    Order.find(function(err,order){
+        if(err){
+            console.log("err");
+        }else{
+            order.forEach(function(order){
+                ordertransfername = order.ordername;
+                ordertransferprice = order.orderprice;
+    
+                var allorder = new Allorder({
+                    allordername : ordertransfername,
+                    allorderprice : ordertransferprice,
+                    token : Token
+                })
+                allorder.save(function(err){
+                    if(err){
+                        console.log(err);
+                }else{
+                    console.log("saved to allorders");
+                  
+                }
+                })
+            });
+        }
+   
+        
+    });
+    res.render("checkout" , { token : Token });
+});
+    
+
+    app.get("/admin",(req,res)=>{
+        res.render("admin");
+    });
+
+    app.get("/food",(req,res)=>{
+        res.render("food")
+    });
+
+    app.post("/food",(req,res)=>{
+        foodinputname = req.body.foodname;
+        foodinputprice = req.body.foodprice;
+        
+        var food = new Food({
+            name : foodinputname,
+            price : foodinputprice
+            
+        });
+
+        food.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                alert("food is added in database");
+                res.redirect("food");
+            }
+        });
+    });
+
+    app.get("/allorders",(req,res)=>{
+        res.render("allorders")
+    });
+
+    app.get("/stock",(req,res)=>{
+        res.render("stock");
+    });
+
+    app.get("/index/:id",(req,res)=>{
+        Food.findById(req.params.id,function(err,Foods){
+            orderfoodinputname = Foods.name;
+            orderfoodinputprice = Foods.price;
+
+            var order = new Order({
+                ordername : orderfoodinputname,
+                orderprice : orderfoodinputprice
+            })
+
+            order.save(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("data successfully added to orders");
+                    alert("added");
+                    res.redirect("/index");
+                }
+            });
+
+        });
+    });
+
+
+    app.delete("/:id",(req,res)=>{
+        Order.findByIdAndRemove(req.params.id,function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log("deleted");
+                res.redirect("/cart");
+            }
+        })
+    });
+
+
+// require("./app/routes.js")(app);
 
 
 app.listen(port);
